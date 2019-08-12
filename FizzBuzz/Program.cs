2834ba47ccea7b728ab70d98742baa61315abd49
Subah Khan <subahkhan@hotmail.com>
2019-08-12 14:10:01 +0100

    fizzBuzz Cata Files Added

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i%3 == 0 && i%5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                if (i%3 == 0 || i.ToString().Contains("3"))
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i%5 == 0 || i.ToString().Contains("5"))
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
